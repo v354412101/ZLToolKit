dd2302eb508df251fbf99bc8f1b2b8303d1fa0a0
Zhou Weimin <zhouweimin@supremind.com>
2021-06-14 07:05:48 +0000

    remove HAVE_SEM

--- src/Thread/semaphore.h
+++ src/Thread/semaphore.h
@@ -11,14 +11,6 @@
 #ifndef SEMAPHORE_H_
 #define SEMAPHORE_H_
 
-/*
- * 目前发现信号量在32位的系统上有问题，
- * 休眠的线程无法被正常唤醒，先禁用之
- #if defined(__linux__)
-#include <semaphore.h>
-#define HAVE_SEM
-#endif //HAVE_SEM
-*/
 #include <atomic>
 #include <mutex>
 #include <condition_variable>
@@ -29,23 +21,13 @@ namespace toolkit {
 class semaphore {
 public:
     explicit semaphore(unsigned int initial = 0) {
-#if defined(HAVE_SEM)
-        sem_init(&_sem, 0, initial);
-#else
         _count = 0;
-#endif
     }
+    
     ~semaphore() {
-#if defined(HAVE_SEM)
-        sem_destroy(&_sem);
-#endif
     }
+    
     void post(unsigned int n = 1) {
-#if defined(HAVE_SEM)
-        while (n--) {
-            sem_post(&_sem);
-        }
-#else
         unique_lock<mutex> lock(_mutex);
         _count += n;
         if(n == 1){
@@ -53,28 +35,18 @@ public:
         }else{
             _condition.notify_all();
         }
-#endif
-
     }
     void wait() {
-#if defined(HAVE_SEM)
-        sem_wait(&_sem);
-#else
         unique_lock<mutex> lock(_mutex);
         while (_count == 0) {
             _condition.wait(lock);
         }
         --_count;
-#endif
     }
 private:
-#if defined(HAVE_SEM)
-    sem_t _sem;
-#else
     int _count;
     mutex _mutex;
     condition_variable_any _condition;
-#endif
 };
 
 } /* namespace toolkit */
